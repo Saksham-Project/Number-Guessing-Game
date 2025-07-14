e6387a153f89d02f1e87ca7809a5ee72fd1bc9d8
Saksham-Project <sakshamthapamagar07@gmail.com>
2025-07-14 22:20:13 +0545

    number guess game

---
+++ num-guss-game.py
@@ -0,0 +1,30 @@
+'''
+Number Guessing Game
+'''
+import random
+    
+ran_num = random.randint(1,100)
+while True:
+    user = input("Enter a number between 1 to 100('e' for exit):")
+
+    if user == 'e':
+        break
+    try:
+         user_choice = int(user)
+    except:
+        print("INVALID INPUT , Please enter a number or 'e' to exit.")
+        continue
+         
+    if ran_num == user_choice:
+        print("CONGRATULATION CORRECT ANSWER!")
+        print("THANK YOU FOR PLAYING THE GAME 💐 ")
+        break
+    elif ran_num > user_choice:
+        print("Given Number Is Small, Please Enter Greater Number.")
+    elif ran_num < user_choice:
+        print("Given Number Is Greater, Please Enter Smaller Number.")
+    
+    
+print("----GAME OVER----")
+
+
